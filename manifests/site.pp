c4b2fdf346de8b27f94c729ab4335f6f1b7e1df0
Ian Ohr <ian@crater.drakenhof>
2020-06-29 13:35:17 +0100

    testing error of duplicating a file resource for the same file.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,13 @@
 node default {
+
   file {'/root/README':
     ensure  => file,
     content => 'this is a readme.',
     owner   => 'root',
   }
+
+  file {'/root/README':
+    owner => 'root',
+  }
+
 }
