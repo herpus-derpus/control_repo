ceafe4dea7c7d65d290f4dea000de519265f289c
Ian Ohr <ian@crater.drakenhof>
2020-07-01 18:44:56 +0100

    missing colon at end of line for file Resource Declaration.

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,7 +4,7 @@ class minecraft {
     ensure => directory,
   }
 
-  file {'/opt/minecraft/minecraft_server.jar'
+  file {'/opt/minecraft/minecraft_server.jar':
     ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/a412fd69db1f81db3f511c1463fd304675244077/server.jar',
   }
@@ -27,5 +27,5 @@ class minecraft {
     ensure => running,
     enable => true,
   }
-  
+
 }
